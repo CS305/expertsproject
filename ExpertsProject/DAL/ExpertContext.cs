db966b9de47cc0a4cf48c539dd3584be1281c439
cs305 <compsci305@gmail.com>
2016-10-28 00:13:55 -0400

    git add*

---
+++ ExpertsProject/DAL/ExpertContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ExpertsProject.Models;
+
+namespace ExpertsProject.DAL
+{
+    public class ExpertContext : DbContext
+    {
+        public ExpertContext() : base("ExpertContext")
+        {
+
+        }
+
+        public DbSet<Expert> Experts { get; set; }
+        public DbSet<Expertise> Expertises { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ExpertsProject/DAL/ExpertInit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ExpertsProject.Models;
+
+namespace ExpertsProject.DAL
+{
+    public class ExpertInit : System.Data.Entity. DropCreateDatabaseIfModelChanges<ExpertContext>
+    {
+        protected override void Seed(ExpertContext context)
+        {
+            var experts = new List<Expert>
+            {
+
+            };
+            context.SaveChanges();
+        }
+    }
+}
