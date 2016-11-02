ae82e642a639ee5ce94f531316107bcea43364c5
CS305 <compscience@gmail.com>
2016-11-02 13:11:11 -0400

    user messages model add

--- ExpertsProject/Models/UserMessagesModel.cs
+++ ExpertsProject/Models/UserMessagesModel.cs
@@ -7,7 +7,15 @@ namespace ExpertsProject.Models
 {
     public class UserMessagesModel
     {
-
+        
+        public String userID { get; set; }
+        public DateTime postDate { get; set; }
+        public dateTime dateOfLastMessage { get; set; }
+        public String subject { get; set; }
+        public String messageID { get; set; }
+        public String body { get; set; }
+        public String ResponseID { get; set; }
+        public DateTime currentDate { get; set; }
 
 
     }
