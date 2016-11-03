2e5181181e5ad0c6bfdf1f4132312d6cd11cc357
cs305 <compsci305@gmail.com>
2016-11-02 21:26:50 -0400

    git add*

--- ExpertsProject/Models/Expert.cs
+++ ExpertsProject/Models/Expert.cs
@@ -9,21 +9,5 @@ namespace ExpertsProject.Models
     {
         public int ID { get; set; }
         public string Category { get; set; }
-        //public string FName { get; set; }
-        //public string LName { get; set; }
-        //public string ExpertiseID { get; set; }
-        //public string Email { get; set; }
-        //public int PhoneNumber { get; set; }
-        //public IsVerified IsVerified { get; set; }
-        //public Prefix Prefix { get; set; }
     }
-
-    //public enum Prefix
-    //{
-        //None = 0,
-        //Dr,
-        //Mr,
-        //Ms,
-        //Mrs
-    //}
 }
