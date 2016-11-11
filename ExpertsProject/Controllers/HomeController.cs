df720dec9500575dc7337973382b709daf5899b6
CS305 <compscience305@gmail.com>
2016-11-11 13:25:59 -0500

    git add *

--- ExpertsProject/Controllers/HomeController.cs
+++ ExpertsProject/Controllers/HomeController.cs
@@ -1,7 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using IdentitySample.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
 
 namespace IdentitySample.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -22,6 +36,63 @@ namespace IdentitySample.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public HomeController()
+        {
+        }
+
+        public HomeController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public string getFirstName()
+        {
+            string firstName = "";
+            var con = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+            using (SqlConnection myConnection = new SqlConnection(con))
+            {
+                string oString = "Select firstName from AspNetUsers";
+                SqlCommand oCmd = new SqlCommand(oString, myConnection);
+                myConnection.Open();
+                using (SqlDataReader oReader = oCmd.ExecuteReader())
+                {
+                    while (oReader.Read())
+                    {
+                        firstName = oReader["firstName"].ToString();
+                    }
+                    myConnection.Close();
+                }
+            }
+            return firstName;
         }
     }
 }
