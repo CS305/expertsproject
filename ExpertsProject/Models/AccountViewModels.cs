d268480ef3d17ec85537d7569f2e917d3ab83e78
CS305 <compscience@gmail.com>
2016-11-28 14:19:27 -0500

    git add*

--- ExpertsProject/Models/AccountViewModels.cs
+++ ExpertsProject/Models/AccountViewModels.cs
@@ -79,7 +79,7 @@ namespace IdentitySample.Models
         public string ConfirmPassword { get; set; }
         public string Address { get; set; } 
         public string City { get; set; } 
-        public string State { get; set; } 
+        public stateList State { get; set; } 
         [Display(Name = "Postal Code")] 
         public string PostalCode { get; set; } 
         [Display(Name = "First Name")]
